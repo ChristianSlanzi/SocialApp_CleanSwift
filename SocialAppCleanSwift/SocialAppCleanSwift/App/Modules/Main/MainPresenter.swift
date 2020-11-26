ea87e275e484ad421d5009ebc3b8ee903394f47e
Christian Slanzi <christian.slanzi@gmail.com>
2020-11-26 17:04:34 +0100

    headers cleanup

--- SocialAppCleanSwift/SocialAppCleanSwift/App/Modules/Main/MainPresenter.swift
+++ SocialAppCleanSwift/SocialAppCleanSwift/App/Modules/Main/MainPresenter.swift
@@ -3,10 +3,7 @@
 //  SocialAppCleanSwift
 //
 //  Created by Christian Slanzi on 26.11.20.
-//  Copyright (c) 2020 ARI MUNANDAR. All rights reserved.
-//  Modify By:  * Ari Munandar
-//              * arimunandar.dev@gmail.com
-//              * https://github.com/arimunandar
+//  
 
 import UIKit
 

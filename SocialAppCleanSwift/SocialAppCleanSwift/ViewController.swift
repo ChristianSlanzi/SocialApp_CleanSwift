f32ff63e84cb3d71dd0868e665c4f734b993435b
Christian Slanzi <christian.slanzi@gmail.com>
2020-11-26 11:51:28 +0100

    new Xcode project

---
+++ SocialAppCleanSwift/SocialAppCleanSwift/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocialAppCleanSwift
+//
+//  Created by Christian Slanzi on 26.11.20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
